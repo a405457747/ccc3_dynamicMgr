ed791d843e639a47d54a23edaae4b5807ed6de69
Sky.Allen <894982165@qq.com>
2024-05-12 20:21:23 +0800

    2

---
+++ assets/DynamicMgr/PanelShowHide.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('PanelShowHide')
+export class PanelShowHide extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
